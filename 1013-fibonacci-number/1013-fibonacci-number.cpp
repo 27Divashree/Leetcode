bcb90cfe09032daa178ccadf9dbbd6b6287f94e6
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-06-10 17:07:23 +0530

    Time: 0 ms (100%), Space: 6 MB (25.21%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fibonacci(int n, vector<int> &dp)
+    {
+        if(n<=1)
+            return n;
+
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        dp[n] = fibonacci(n-1,dp) + fibonacci(n-2,dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n+1);
+        for(int i=0;i<=n;i++)
+            dp[i] = -1;
+        return fibonacci(n,dp);
+    }
+};
