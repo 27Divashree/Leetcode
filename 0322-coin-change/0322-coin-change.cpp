b61482a3eadda4301f6fcf47f09ebe70719f7be8
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-06-11 11:58:48 +0530

    Time: 150 ms (30.66%), Space: 14.5 MB (70.99%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int solveTab(vector<int> &num, int x)
+{
+    vector<int> dp(x+1,INT_MAX);
+    dp[0] = 0;
+    for(int i=0;i<=x;i++)
+    {
+        for(int j=0;j<num.size();j++)
+        {
+            if(i-num[j]>=0 && dp[i-num[j]]!=INT_MAX)
+            {
+                dp[i] = min(dp[i], 1+dp[i-num[j]]);
+            }
+        }
+    }
+    if(dp[x]==INT_MAX)
+        return -1;
+    
+    return dp[x];
+}
+    int coinChange(vector<int>& coins, int amount) {
+        return solveTab(coins,amount);
+    }
+};
