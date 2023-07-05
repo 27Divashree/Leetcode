70b46b947a77ba901dea8a78707ce89ff7b08bf7
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-07-05 12:32:14 +0530

    Time: 19 ms (46.98%), Space: 6.4 MB (97.92%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -70,10 +70,46 @@ public:
         return dp[0][0];
     }
 
+    int solveSO(string& a,string& b)
+    {
+        vector<int> curr(b.length()+1,0);
+        vector<int> next(b.length()+1,0);
+
+        for(int j=0;j<b.length();j++)
+            next[j] = b.length()-j;
+        
+        for(int i=a.length()-1;i>=0;i--)
+        {
+            for(int j=b.length()-1;j>=0;j--)
+            {
+                curr[b.length()] = a.length()-i;
+                int ans = 0;
+                if(a[i]==b[j])
+                    ans = next[j+1];
+                else
+                {
+                    int insertAns = 1+curr[j+1];
+                    int deleteAns = 1+next[j];
+                    int replaceAns = 1+next[j+1];
+                    ans = min(insertAns,min(deleteAns,replaceAns));
+                }
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+
     int minDistance(string word1, string word2) {
         //return solve(word1,word2,0,0);
-        vector<vector<int>> dp(word1.length(),vector<int>(word2.length(),-1));
+        //vector<vector<int>> dp(word1.length(),vector<int>(word2.length(),-1));
         //return solveMem(word1,word2,0,0,dp);
-        return solveTab(word1,word2);
+        //return solveTab(word1,word2);
+
+        if(word1.length()==0)
+            return word2.length();
+        if(word2.length()==0)
+            return word1.length();
+        return solveSO(word1,word2);
     }
 };
