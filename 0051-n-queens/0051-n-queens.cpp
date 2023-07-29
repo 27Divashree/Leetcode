a3ad5f39261e6b481645cfafaeeb6a316aa00ae2
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-07-29 11:32:37 +0530

    Time: 0 ms (100%), Space: 7.2 MB (67.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,52 +1,54 @@
 class Solution {
 public:
-    vector<vector<string>> res;
-    bool isSafe(vector<string> &board, int x, int y){
-        for(int row = 0; row < x; row++){
-            if(board[row][y] == 'Q'){
+    vector<vector<string>> result;
+    bool isSafe(vector<string> &board,int x,int y)
+    {
+        for(int row = 0;row<x;row++)
+        {
+            if(board[row][y]=='Q')
                 return false;
-            }
         }
-        int row = x;                             //for upper left diagonal
-        int col = y;
-        while(row >= 0 && col >= 0){
-            if(board[row][col] == 'Q'){
+
+        int row = x, col = y;
+        while(row>=0 && col>=0){
+            if(board[row][col] =='Q')
                 return false;
-            }
             row--;
             col--;
         }
-        row = x;                                //for upper right diagonal
+        row = x;
         col = y;
-        while(row >= 0 && col < board.size()){
-            if(board[row][col] == 'Q'){
+        while(row>=0 && col<board.size()){
+            if(board[row][col] =='Q')
                 return false;
-            }
             row--;
             col++;
         }
         return true;
     }
-    void NQueen(vector<string> &board, int x){
-        if(x == board.size()){
-            res.push_back(board);
+    void NQueen(vector<string> &board,int x)
+    {
+        if(x==board.size())
+        {
+            result.push_back(board);
             return;
         }
-        for(int col = 0; col < board.size(); col++){
+
+        for(int col=0;col<board.size();col++)
+        {
             if(isSafe(board,x,col)){
                 board[x][col] = 'Q';
-                
+
                 NQueen(board,x+1);
-                board[x][col] = '.';                //backtracking
+                board[x][col] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        if(n <= 0){
-            return {{}};
-        }
+        if(n<=0)
+            return {};
         vector<string> board(n,string(n,'.'));
         NQueen(board,0);
-        return res;
+        return result;
     }
 };
