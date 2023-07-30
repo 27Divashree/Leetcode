34ae34adc23e4d1c0b5f4572cd9e17f4de5b7801
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-07-30 10:52:24 +0530

    Time: 505 ms (14.3%), Space: 52.6 MB (5.87%) - LeetHub

--- 0923-super-egg-drop/0923-super-egg-drop.cpp
+++ 0923-super-egg-drop/0923-super-egg-drop.cpp
@@ -43,15 +43,46 @@ public:
         return dp[k][n]=ans;
     }
 
-    /*int solveTab(int k,int n)
+    int solveTab(int k,int n)
     {
+        if(n==0 || n==1)
+            return n;
+        if(k==1)
+            return n;
+
         vector<vector<int>> dp(k+1, vector<int>(n+1,0));
-        for(int)
-    }*/
+        for(int i=0;i<=k;i++)
+            dp[i][0] = 0, dp[i][1] = 1;
+        for(int i=0;i<=n;i++)
+        dp[0][i] = 0, dp[1][i] =i;
+
+        for(int i=2;i<k+1;i++)
+        {
+            for(int j=2;j<n+1;j++)
+            {
+                int ans = 1000000, l=1,h=j,temp=0;
+                while(l<=h)
+                {
+                    int mid = l + (h-l)/2;
+                    int left = dp[i-1][mid-1];
+                    int right = dp[i][j-mid];
+                    temp = 1 + max(left,right);
+                    if(left<right)
+                        l = mid + 1;
+                    else
+                        h = mid - 1;
+                    ans = min(ans,temp);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[k][n];
+    }
 
     int superEggDrop(int k, int n) {
         //return findRec(k,n);
         vector<vector<int>> dp(k+1, vector<int>(n+1,-1));
-        return solveMem(k,n,dp);
+        //return solveMem(k,n,dp);
+        return solveTab(k,n);
     }
 };
