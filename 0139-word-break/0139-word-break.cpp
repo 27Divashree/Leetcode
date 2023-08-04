e20349fcd57b334ed28961c557363c4eccdcdaa5
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-08-04 11:32:15 +0530

    Time: 22 ms (34.52%), Space: 14.8 MB (17.49%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    bool wordBreak(string s,unordered_set<string> &set,vector<int> &dp,int start)
+    bool solve(string s,unordered_set<string> &set,vector<int> &dp,int start)
     {
-        if(s.size()==start)
+        if(start==s.size())
             return true;
-        
         if(dp[start]!=-1)
             return dp[start];
         
         for(int i=start;i<s.size();i++)
         {
-            if(set.count(s.substr(start,i+1-start)) && wordBreak(s,set,dp,i+1)){
+            if (set.count(s.substr(start,i+1-start)) && solve(s,set,dp,i+1))
+            {
                 dp[start] = true;
                 return true;
             }
         }
 
-        return dp[start]=false;
+        return dp[start] = false;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> set(wordDict.begin(),wordDict.end());
+        unordered_set<string> sets(wordDict.begin(),wordDict.end());
         vector<int> dp(s.size(),-1);
-        return wordBreak(s,set,dp,0);
+        return solve(s,sets,dp,0);
     }
 };
