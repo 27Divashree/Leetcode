1990786a91bb2ee2049af7cf24008c1175108b4e
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-09-21 11:04:21 +0530

    Time: 24 ms (61.39%), Space: 71.6 MB (12.37%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,59 +10,40 @@
  */
 class Solution {
 public:
-    ListNode *addTwoNumbersHelper(ListNode *node1, ListNode *node2, int carry)
+    ListNode* helper(ListNode *l1, ListNode *l2,int carry)
     {
-        if (node1 == NULL && node2 == NULL && carry == 0)
-        {
-            // Added both the lists and no carry is generated.
+        if(l1==NULL && l2==NULL && carry==0)
             return NULL;
-        }
-
-        // 'val1' and 'val2' store the data of the current nodes of the two linked lists.
         int val1, val2;
-
-        // 'next1' and 'next2' point to the node present after the current nodes of the two linked lists.
         ListNode *next1, *next2;
-
-        if (node1 == NULL)
+        if(l1==NULL)
         {
-            // List 1 is empty. So, consider the current node's value as zero.
             val1 = 0;
-            // And the node after the current node as NULL.
             next1 = NULL;
         }
         else
         {
-            val1 = node1->val;
-            next1 = node1->next;
+            val1 = l1->val;
+            next1 = l1->next;
         }
-
-        if (node2 == NULL)
+        if(l2==NULL)
         {
-            // List 2 is empty. So, consider the current node's value as zero.
             val2 = 0;
-            // And the node after the current node as NULL.
             next2 = NULL;
         }
         else
         {
-            val2 = node2->val;
-            next2 = node2->next;
+            val2 = l2->val;
+            next2 = l2->next;
         }
 
-        // Add the values in the current nodes along with the carry.
-        int sum = val1 + val2 + carry;
+        int sum = val1+val2+carry;
+        ListNode* sumNode = new ListNode(sum%10);
 
-        // Create the next node of the sum list.
-        ListNode *sumNode = new ListNode(sum % 10);
-
-        // Recursively call the function to generate the remaining nodes of the sum list.
-        sumNode->next = addTwoNumbersHelper(next1, next2, sum / 10);
-
+        sumNode->next = helper(next1,next2,sum/10);
         return sumNode;
     }
-
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return addTwoNumbersHelper(l1,l2,0);
+        return helper(l1,l2,0);
     }
 };
