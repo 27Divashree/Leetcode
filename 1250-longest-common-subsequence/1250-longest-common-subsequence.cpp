0bf3bb0dd7e7075b06519d4d293fa65cf5057b52
Divashree Agarwal <93401095+27Divashree@users.noreply.github.com>
2023-09-24 19:32:41 +0530

    Time: 32 ms (82.57%), Space: 6.8 MB (89.63%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int solveTab(string a, string b)
+    int solve(string s, string t)
     {
-        vector<int> curr(b.length()+1,0);
-        vector<int> next(b.length()+1,0);
-
-        for(int i=a.length()-1;i>=0;i--)
+        vector<int> currRow(t.length()+1,0);//dp[i]
+        vector<int> nextRow(t.length()+1,0);//dp[i+1]
+        
+        for(int i=s.length()-1;i>=0;i--)
         {
-            for(int j=b.length()-1;j>=0;j--)
+            for(int j=t.length()-1;j>=0;j--)
             {
                 int ans = 0;
-                if(a[i]==b[j])
-                    ans = 1+next[j+1];
-                else
-                {
-                    ans = max(next[j],curr[j+1]);
-                }
-                curr[j] = ans;
+                if(s[i]==t[j])
+                ans = 1 + nextRow[j+1];
+            else
+                ans = max(nextRow[j],currRow[j+1]);
+                currRow[j] = ans;
             }
-            next = curr;
+            nextRow = currRow;
         }
-        return next[0];
-
+        
+        return nextRow[0];
     }
     int longestCommonSubsequence(string text1, string text2) {
-        return solveTab(text1,text2);
+        //vector<vector<int>> dp(text1.length(),vector<int>(text2.length(),-1)) ;
+        return solve(text1,text2);
     }
 };
